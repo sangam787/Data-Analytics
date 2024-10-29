dc0cb6fdb57e2306b1ac1ad80b538a60586f319a
sangam787 <deepfact12@gmail.com>
2024-10-29 18:40:44 +0530

    all que added

--- basicAsgmt.py
+++ basicAsgmt.py
@@ -1,1 +1,21 @@
 #Question-1 Explain the key features of Python that make it a popular choice for programming.
+"""
+Answer-1
+Python is the one of the most popular programming language. 
+Below, its some key feature described why it is popular.
+
+1-Python is easiest programing language, it is very easy to read and understand. Its syntax are very simple similar like english words.
+2-Python has one of the largest collections of free libraries.
+3-Python is free to use. We can download and modify.
+4-Python is designed around classes and object, which allows for reusable code.
+5-Python is a High Level Language.
+6-Python is portable and integrated programing language. 
+"""
+# _________________________________________________________________________________________________
+#Question-2 Describe the role of predefined keywords in Python and provide examples of how they are used in a program.
+#Question-3 Compare and contrast mutable and immutable objects in Python with examples.
+#Question-4 Discuss the different types of operators in Python and provide examples of how they are used.
+#Question-5 Explain the concept of type casting in Python with examples.
+
+#Question-6 How do conditional statements work in Python? Illustrate with examples.
+#Question-7 Describe the different types of loops in Python and their use cases with examples.

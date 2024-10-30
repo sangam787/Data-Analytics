9ea6b08accedb2b355c778f471a694d8e6d9569b
sangam787 <deepfact12@gmail.com>
2024-10-30 15:06:19 +0530

    modified

--- basicAsgmt.py
+++ basicAsgmt.py
@@ -13,6 +13,35 @@ Below, its some key feature described why it is popular.
 """
 # _________________________________________________________________________________________________
 #Question-2 Describe the role of predefined keywords in Python and provide examples of how they are used in a program.
+"""
+Answer-2
+There are a set of keywords that are reserved words in Python. These words can not be used as variable names, function names, or any other identifiers.
+The role of predefined keywords are for working on programs that have a specific meaning.
+Now we are going to describe the keywords and their roles with examples.
+(1)- and (A logical operator):- Return True if both statements are True:
+example- 
+x = (3 < 7 and 5 < 8)
+print(x)
+
+(2)- as (To create an alias):-Refer to the calendar module as c:
+import calendar as n
+
+print(n.month_name[4])
+
+(2)- elif (Used in conditional statements, same as else if)
+light = input("enter color name: ")
+if light == "red":
+    print("stop")
+elif light == "green":
+    print("go")
+elif light == "yellow":
+    print("wait")
+
+"""
+
+
+
+
 #Question-3 Compare and contrast mutable and immutable objects in Python with examples.
 #Question-4 Discuss the different types of operators in Python and provide examples of how they are used.
 #Question-5 Explain the concept of type casting in Python with examples.
